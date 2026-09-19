b542c1f88eeb29533b34db54e14b39845598dc35
ToxicH3ro <margonmg4@gmail.com>
2026-09-19 17:44:41 -0600

    Redirigir inicio a login

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Veterinaria San Roque</h1>
-      <p>Bienvenido a nuestra veterinaria</p>
-    </main>
-  );
+import { redirect } from "next/navigation";
+
+export default function HomePage() {
+  redirect("/login");
 }
